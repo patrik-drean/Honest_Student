c2a63da1a4f5fee059fda7ecc91163074da55acf
patrik-drean <patrikdrean@gmail.com>
2018-03-09 15:45:18 -0700

    added db url in settings.py

--- honest_student/settings.py
+++ honest_student/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import shutil
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,6 +117,8 @@ WSGI_APPLICATION = 'honest_student.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
+
+# From tutorial
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
