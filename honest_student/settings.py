63117da2f58c69a188519754eb45b5a927500986
patrik-drean <patrikdrean@gmail.com>
2018-03-09 15:47:48 -0700

    added db url in settings.py

--- honest_student/settings.py
+++ honest_student/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import shutil
-import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
-DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
